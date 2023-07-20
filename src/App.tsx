3308789b2dac5c0d7f35d1820aae74dae6d92b7e
Peter Hueer <peter.huer@unicepta.com>
2023-07-20 16:17:13 +0200

    Better animations

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,43 @@ import { motion } from 'framer-motion';
 
 export default function App() {
   return (
-    <div className="grid place-items-center h-screen pt-20">
+    <div className="grid place-items-center h-screen">
+      <motion.a
+        href="https://github.com/PeterHueer/framer-motion-test"
+        className="uppercase hover:text-white rounded bg-secondary p-1 pr-2 pl-2 text-white m-5"
+        whileHover={{ scale: 1.2 }}
+        transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+        target="_blank"
+      >
+        Github link
+      </motion.a>
       <div className="w-full max-w-screen-xl text-white">
         <motion.div
           className="grid grid-cols-3 gap-5"
           variants={{
             show: {
               transition: {
-                staggerChildren: 0.2,
+                staggerChildren: 0.1,
+                delayChildren: 0.5,
               },
             },
           }}
           initial="hidden"
           whileInView="show"
         >
-          {Array.from(Array(12).keys()).map(id => (
+          {Array.from(Array(9).keys()).map(id => (
             <Card
               key={id}
               variants={{
-                hidden: { opacity: 0, y: 30 },
+                hidden: { opacity: 0.001, y: 100, scale: 0.5 },
                 show: {
                   y: 0,
                   opacity: 1,
+                  scale: 1,
                   transition: {
-                    staggerChildren: 0.2,
+                    y: {
+                      duration: 0.5,
+                    },
                   },
                 },
               }}
